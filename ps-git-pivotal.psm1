935a8758ecb024b18140ce47afbff3fa7e48c847
marcio.ide <marcio.ide@livegroup.com.br>
2015-10-01 18:50:32 -0300

    starting a story

--- ps-git-pivotal.psm1
+++ ps-git-pivotal.psm1
@@ -1,28 +1,57 @@
-function Start-Feature
+function Start-Story
 {
 	CheckPivotalConfig
 	ListLastFiveHistories
 }
 
 function ListLastFiveHistories {
-	
-	$stories = GetData "stories?limit=5&with_state=unstarted"
-
+	$stories = GetData "/stories?limit=5&with_state=unstarted"
 	Foreach ($story in $stories) {
-		Write-Host ("({0}) {1} - {2}" -f $story.story_type, $story.id, $story.name) 
+		Write-Host ("[{0}] {1} - {2}" -f $story.story_type, $story.id, $story.name) 
 	}
 	Write-Host ""
+	$selectedHistoryId = Read-Host "Please tell me the history id that you want to start working"
+	CreateFeatureBranch $selectedHistoryId
+}
+
+function CreateFeatureBranch($selectedHistoryId) {
+	$endpoint = ("/stories/{0}" -f $selectedHistoryId)
+	$story = GetData $endpoint
+	$storyType = $story.story_type
+	Write-Host ("Creating your new {0} branch" -f $storyType)
+	git checkout -b ("{0}-{1}" -f $storyType, $story.id)
+	PutData $endpoint "{`"current_state`":`"started`"}"
+}
+
+function PutData($endpoint, $data) {
+	$webClient = GetWebClient
+	$url = GetUrl $endpoint
+	$webClient.UploadString($url, 'PUT', $data)
 }
 
 function GetData($endpoint) {
-	$pivotalApiToken = Invoke-Expression 'git config --get pivotal.api-token 2>$null'
+	$webClient = GetWebClient
+	$url = GetUrl $endpoint
+	$data = $webClient.DownloadString($url)
+	return $data | ConvertFrom-Json
+}
+
+function GetUrl($endpoint) {
+	$baseUrl = GetBaseUrl
+	return ("{0}{1}" -f $baseUrl, $endpoint)
+}
+
+function GetBaseUrl() {
 	$pivotalProjectId = Invoke-Expression 'git config --get pivotal.project-id 2>$null'
-	$url = ("https://www.pivotaltracker.com/services/v5/projects/{0}/{1}" -f $pivotalProjectId, $endpoint)
+	return ("https://www.pivotaltracker.com/services/v5/projects/{0}" -f $pivotalProjectId)
+}
+
+function GetWebClient($endpoint) {
+	$pivotalApiToken = Invoke-Expression 'git config --get pivotal.api-token 2>$null'
 	$webClient = new-object System.Net.WebClient
 	$webClient.headers.Add('X-TrackerToken', $pivotalApiToken)
-	$data = $webClient.DownloadString($url)
-	
-	return $data | ConvertFrom-Json
+	$webClient.headers.Add('Content-Type', 'application/json')
+	return $webClient
 }
 
 function CheckPivotalConfig {
